b5df01a188f9f36b1d8642b54f6fb05cb1d99b7b
Kiran571 <kiranvsuryavanshi05@gmail.com>
2023-12-13 23:31:48 +0530

    tut21 edits

--- jwt-ui/src/app/login/login.component.ts
+++ jwt-ui/src/app/login/login.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+  login(form: NgForm) {
+    console.log(form.value);
+  }
+
 
 }
